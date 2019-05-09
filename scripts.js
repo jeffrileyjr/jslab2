64153a771d2972c9935878e4995b30af28cd646a
jeff <jeffrileyjr80@live.com>
2019-05-08 23:01:02 -0400

    first commit for part 2

--- scripts.js
+++ scripts.js
@@ -1,50 +1,4 @@
 "use strict";
-
-class AddressBook {
-  constructor() {
-    this.contacts = [
-      {
-        name: "Homer J. Simpson",
-        email: "homer.simpson@springfieldnuclear.gov",
-        phone: "KL5-3226",
-        relation: "TV-dad"
-      },
-      {
-        name: "Bob Ross",
-        email: "bob.ross@pbs.org",
-        phone: "555-555-5555",
-        relation: "chill dude"
-      },
-      {
-        name: "Bruce Wayne",
-        email: "bruce@wayne-enterprises.com",
-        phone: "734-555-1234",
-        relation: "Batman"
-      }
-    ];
-  }
-  add(info) {
-    let newContact = new Contact(info.name, info.email, info.phone, info.relation);
-    this.contacts.push(newContact);
-  }
-  deleteAt(index) {
-    this.contacts.splice(index, 1);
-  }
-  print() {
-    for (let i = 0; i < this.contacts.length; i++) {
-      console.log(`Name: ${this.contacts[i].name} Email: ${this.contacts[i].email} Phone: ${this.contacts[i].phone} relation: ${this.contacts[i].relation}`)
-    }
-  }
-  deleteByName(name) {
-    for (let i = 0; i < this.contacts.length; i++) {
-      if (name === this.contacts[i].name) {
-        console.log(`${this.contacts[i].name} will be deleted.`);
-        this.contacts.splice(i, 1);
-      }
-    }
-  }
-}
-
 class Contact {
   constructor(name, email, phone, relation) {
     this.name = name;
@@ -54,32 +8,55 @@ class Contact {
   }
 }
 
-let addressBook = new AddressBook();
-
-
-while (true) {
-  let userChoice = prompt("Welcome to your address book!\nWould you like to add, delete, print or quit?");
-  if (userChoice === "add") {
-    let info = {
-      name: prompt("What is the contact's name?"),
-      email: prompt(`What is contacts's E-mail address?`),
-      phone: prompt(`What is contacts's phone number?`),
-      relation: prompt(`What is contacts's relation?`),
-    };
-    addressBook.add(info);
-  } else if (userChoice === "delete") {
-    let deleteChoice = prompt(`Would you like to delete by index or name?`);
-    if (deleteChoice === "index") {
-      let index = prompt(`Which index between 0 and ${addressBook.contacts.length - 1} would you like to delete?`);
-      addressBook.deleteAt(index);
-    } else if (deleteChoice === "name") {
-      let name = prompt(`What contact would you like to remove?`);
-      addressBook.deleteByName(name);
+class AddressBook {
+  constructor() {
+    this.contacts = [
+      new Contact("Homer Simpson", "homer@springfieldnuclear.gov", "111-222-3333", "TV Dad"),
+      new Contact("Bruce Wayne", "bruce@wayne-enterprises.com", "333-555-9999", "Batman"),
+      new Contact("Scrooge McDuck", "richduck@scrooge.com", "333-555-9999", "Rich Duck"),
+    ];
+  }
+  addContact(event) {
+    event.preventDefault();
+    let contactInfo = document.querySelectorAll("input");
+    let div = document.createElement("div");
+    div.classList.add("contact_card", "animated", "bounceInDown");
+    div.innerHTML = `
+    <p>Name: ${contactInfo[0].value} </p>
+    <p> Email: ${contactInfo[1].value}</p>
+    <p> Phone: ${contactInfo[2].value}</p>
+    <p> Relation: ${contactInfo[3].value}</p> 
+    <i class="material-icons trash">delete</i>
+    `;
+    document.querySelector(".contact_display").append(div);
+    for (let input of contactInfo) {
+      input.value = "";
+      document.getElementById("name").focus();
     }
-  } else if (userChoice === "print") {
-    addressBook.print();
-  } else if (userChoice === "quit") {
-    console.log("Thank you, good-bye!");
-    break;
+  }
+  deleteAt(event) {
+    if (event.target.tagName === "I") {
+      event.target.parentNode.remove();
+    }
+  }
+  display() {
+    const contactCard = document.querySelector(".contact_display");
+    this.contacts.forEach(contact => {
+      const el = document.createElement("div");
+      el.classList.add("contact_card");
+      el.innerHTML = `
+    <p>Name: ${contact.name} </p>
+    <p> Email: ${contact.email}</p>
+    <p> Phone: ${contact.phone}</p>
+    <p> Relation: ${contact.relation}</p> 
+    <i class="material-icons trash">delete</i>
+    `;
+      contactCard.append(el);
+    })
   }
 }
+const addressBook = new AddressBook();
+document.querySelector("form").addEventListener("submit", addressBook.addContact);
+document.querySelector(".contact_display").addEventListener("click", addressBook.deleteAt);
+
+window.onload = addressBook.display();
